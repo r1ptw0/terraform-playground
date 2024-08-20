8e53486694a1ad43e70b75a3bab89a0b10584b5a
r1ptw0 <rnewman@fortinet.com>
2024-08-20 14:25:24 +0000

    clean up before promo

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "regions" {
   type    = list(string)
-  default = ["ca-central-1", "ca-east-1"]
+  default = ["ca-central-1", "ca-east-1", "us-east-1"]
 }
 variable "prefix" {
   type = string
